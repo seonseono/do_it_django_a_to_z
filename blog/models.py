c8dbf6b4e62195ee37fa140deaf609d0237b6ebc
seonseono <nata0919@likelion.org>
2024-04-01 16:11:52 +0900

    create post model and time setting

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30) # Title field
+    content = models.TextField() # Content field
+
+    created_at = models.DateTimeField(auto_now_add=True) # 처음 레코드가 생성된 시점을 자동으로 입력
+    update_at = models.DateTimeField(auto_now=True) # 레코드가 가장 마지막으로 저장된 시점(수정된 시점)
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
+
