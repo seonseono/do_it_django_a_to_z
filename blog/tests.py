fe623dfb1c104d60688160e57edba7a05ca4bb91
seonseono <nata0919@likelion.org>
2024-04-03 13:39:57 +0900

    write post detail test

--- blog/tests.py
+++ blog/tests.py
@@ -9,14 +9,14 @@ class TestView(TestCase):
 
     def test_post_list(self):
         response = self.client.get('/blog/')
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
-        self.assertEquals(soup.title.text, 'Blog')
+        self.assertEqual(soup.title.text, 'Blog')
         navbar = soup.nav
         self.assertIn('Blog', navbar.text)
         self.assertIn('About Me', navbar.text)
 
-        self.assertEquals(Post.objects.count(), 0)
+        self.assertEqual(Post.objects.count(), 0)
         main_area = soup.find('div', id='main-area')
         self.assertIn('Not yet to post', main_area.text)
 
@@ -28,14 +28,38 @@ class TestView(TestCase):
             title='2nd Post',
             content='1등이 전부는 아니잖아요?',
         )
-        self.assertEquals(Post.objects.count(), 2)
+        self.assertEqual(Post.objects.count(), 2)
 
         response = self.client.get('/blog/')
         soup = BeautifulSoup(response.content, 'html.parser')
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
         main_area = soup.find('div', id='main-area')
         self.assertIn(post_001.title, main_area.text)
         self.assertIn(post_002.title, main_area.text)
 
         self.assertNotIn('Not yet to post', main_area.text)
+
+    def test_post_detail(self):
+        post_001 = Post.objects.create(
+            title='1st Post',
+            content='Hello World. We are the world',
+        )
+
+        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+
+        response = self.client.get(post_001.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        soup = BeautifulSoup('About Me', navbar.text)
+
+        self.assertIn(post_001.title, soup.title.text)
+
+        main_area = soup.find('div', id='main-area')
+        post_area = main_area.find('div', id='post-area')
+        self.assertIn(post_001.title, post_area.text)
+
+        self.assertIn(post_001.content, post_area.text)
