2f92130e83d8bbb3bc2b8e80aaf9cb36c52b329d
seonseono <nata0919@likelion.org>
2024-04-05 11:47:30 +0900

    create new post page

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from . import views
 # 해당 경로 안의 views를 import
 
 urlpatterns = [
+    path('create_post/', views.PostCreate.as_view()),
+    path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Post, Category
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Post, Category, Tag
+
+
+class PostCreate(CreateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
 
 class PostList(ListView):
     model = Post
@@ -39,6 +44,21 @@ def category_page(request, slug):
             'categories' : Category.objects.all(),
             'no_category_post_count' : Post.objects.filter(category=None).count(),
             'category' : category,
+        }
+    )
+
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list' : post_list,
+            'tag' : tag,
+            'categories' : Category.objects.all(),
+            'no_category_post_count' : Post.objects.filter(category=None).count(),
         }
     )
 
