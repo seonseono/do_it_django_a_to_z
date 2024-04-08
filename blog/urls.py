4d786ddb20f16a70d6e4937cd11fe6fc49beca1e
seonseono <nata0919@likelion.org>
2024-04-08 16:50:26 +0900

    add search function

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 # 해당 경로 안의 views를 import
 
 urlpatterns = [
+    path('search/<str:q>/', views.PostSearch.as_view()),
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>/', views.tag_page),
--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
+from django.db.models import Q
 from .models import Post, Category, Tag
-
 
 class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
@@ -130,6 +130,21 @@ def tag_page(request, slug):
         }
     )
 
+class PostSearch(PostList):
+    paginate_by = None
+
+    def get_queryset(self):
+        q = self.kwargs['q']
+        post_list = Post.objects.filter(
+            Q(title__contains=q) | Q(tags__name__contains=q)
+        ).distinct()
+        return post_list
+
+    def get_context_data(self, **kwargs):
+        context = super(PostSearch, self).get_context_data()
+        q = self.kwargs['q']
+        context['search_info'] = f'Search: {q} ({self.get_queryset().count()})'
+        return context
 
 
 # def index(request):
