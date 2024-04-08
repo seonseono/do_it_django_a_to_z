b1a06c4fe9d893fb0caa2ccca82142c3a3c85f42
seonseono <nata0919@likelion.org>
2024-04-08 20:20:56 +0900

    renewal home page

--- single_pages/tests.py
+++ single_pages/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from bs4 import BeautifulSoup
+from blog.models import Post
+
+# Create your tests here.
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user_nina = User.objects.create_user(username='nina', password='somepassword')
+
+    def test_landing(self):
+        post_001 = Post.objects.create(
+            title='1st Post',
+            content='This is a 1st post',
+            author=self.user_nina,
+        )
+
+        post_002 = Post.objects.create(
+            title='2nd Post',
+            content='This is a 2nd post',
+            author=self.user_nina,
+        )
+
+        post_003 = Post.objects.create(
+            title='3rd Post',
+            content='This is a 3rd post',
+            author=self.user_nina,
+        )
+
+        post_004 = Post.objects.create(
+            title='4th Post',
+            content='This is a 4th post',
+            author=self.user_nina,
+        )
+
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        body = soup.body
+        self.assertNotIn(post_001.title, body.text)
+        self.assertIn(post_002.title, body.text)
+        self.assertIn(post_003.title, body.text)
+        self.assertIn(post_004.title, body.text)
 
 # Create your tests here.
--- single_pages/views.py
+++ single_pages/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from blog.models import Post
 
 def landing(request):
+    recent_posts = Post.objects.order_by('-pk')[:3]
     return render(
         request,
         'single_pages/landing.html',
@@ -9,6 +11,9 @@ def landing(request):
         # templates 폴더 안에 single_pages 폴더 생성 후 html 파일 생성
         # single_pages(landing.html 파일이 있는)에 templates 라는 속성이 있기 때문에
         # 이름이 같아도 충돌이 일어나지 않음
+        {
+            'recent_posts': recent_posts,
+        }
     )
 
 def about_me(request):
